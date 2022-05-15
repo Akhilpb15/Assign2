5a9f5ca3ade6f104ed63965fc6732b0e89cb1ae5
Akhilpb15 <104380444+Akhilpb15@users.noreply.github.com>
2022-05-15 17:53:29 +0530

    Add files via upload

---
+++ Assn2 Output 15 may.py
@@ -0,0 +1,57 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======= RESTART: C:/Users/asus/OneDrive/Desktop/PP/Assgn2 Final 15 may.py ======
+-----------QUESTION 1-------------------
+----------Part a-------------------------
+35
+----------Part b-------------------------
+egaugnal evitisnes esac a si nohtyP
+----------Part c-------------------------
+a case sensitive
+----------Part d-------------------------
+Python is object oriented language
+----------Part e-------------------------
+10
+----------Part f-------------------------
+Pythonisacasesensitivelanguage
+
+-----------QUESTION 2-------------------
+Enter the Name: 
+Akhil Makkar
+Enter the SID: 
+21102105
+Enter the department of the student:
+Civil Engg.
+Enter the CGPA of the student:
+9.9
+Hey,Akhil Makkar Here!
+My SID is 21102105
+I am from Civil Engg. Department and my CGPA is 9.9
+
+-----------QUESTION 3-------------------
+----------Part a-------------------------
+8
+----------Part b-------------------------
+58
+----------Part c-------------------------
+50
+----------Part d-------------------------
+224 40
+----------Part e-------------------------
+14 0
+
+-----------QUESTION 4-------------------
+Enter the string: namE
+NO
+
+-----------QUESTION 5-------------------
+Enter side 1: 3
+Enter side 2: 5
+Enter side 3: 9
+YES
+
+-----------QUESTION 6-------------------
+Enter first number:56
+Enter second number:21
+4
